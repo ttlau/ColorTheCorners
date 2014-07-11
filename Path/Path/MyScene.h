c884fba86b347a3548ab198a263ac4a14665d864
Tim Lau <tlau9195@gmail.com>
2014-07-11 15:28:27 -0700

    Spritebuilder Background Edit

---
+++ Path/Path/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Path
+//
+
+//  Copyright (c) 2014 Tim Lau. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Path/Path/main.m
@@ -0,0 +1,18 @@
+	//
+//  main.m
+//  Path
+//
+//  Created by Tim Lau on 7/10/14.
+//  Copyright (c) 2014 Tim Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
