805bbc6bbbac09bf105db41a2c12af36e67242d1
Tim Lau <tlau9195@gmail.com>
2014-08-15 10:48:01 -0700

    learn more button

--- Path.spritebuilder/Source/EndScene.m
+++ Path.spritebuilder/Source/EndScene.m
@@ -12,6 +12,7 @@
 
 @implementation EndScene{
     HTPressableButton *homeButton;
+    HTPressableButton *learnMoreButton;
 }
 
 -(void)didLoadFromCCB{
@@ -34,14 +35,22 @@
     CCDirector *thisDirector = [CCDirector sharedDirector];
     
     //note: for the x coordinate, take the width of the button + displacement from side and then minus width
-    CGRect frame2 = CGRectMake([thisDirector viewSize].width/2 - 87.5, [thisDirector viewSize].height/2 +25, 165, 55);
+    CGRect frame2 = CGRectMake([thisDirector viewSize].width/2 - 87.5, [thisDirector viewSize].height/2 +75.5, 165, 55);
     homeButton = [[HTPressableButton alloc] initWithFrame:frame2 buttonStyle:HTPressableButtonStyleRounded];
-    [homeButton setTitle:@"Return Home" forState:UIControlStateNormal];
+    [homeButton setTitle:@"Home" forState:UIControlStateNormal];
     homeButton.buttonColor = [UIColor ht_sunflowerColor];
     homeButton.shadowColor = [UIColor ht_citrusColor];
     [homeButton addTarget:self action:@selector(home) forControlEvents:UIControlEventTouchUpInside];
     
+    CGRect frame3 = CGRectMake([thisDirector viewSize].width/2 - 87.5, [thisDirector viewSize].height/2 +5, 165, 55);
+    learnMoreButton = [[HTPressableButton alloc] initWithFrame:frame3 buttonStyle:HTPressableButtonStyleRounded];
+    [learnMoreButton setTitle:@"Learn More" forState:UIControlStateNormal];
+    learnMoreButton.buttonColor = [UIColor ht_sunflowerColor];
+    learnMoreButton.shadowColor = [UIColor ht_citrusColor];
+    [learnMoreButton addTarget:self action:@selector(learnMore) forControlEvents:UIControlEventTouchUpInside];
+    
     [[[CCDirector sharedDirector] view]addSubview: homeButton];
+    [[[CCDirector sharedDirector] view]addSubview: learnMoreButton];
 
     
 }
@@ -51,6 +60,13 @@
     CCScene *mainScene = [CCBReader loadAsScene:@"MainScene"];
     [[CCDirector sharedDirector] replaceScene:mainScene withTransition: [CCTransition transitionPushWithDirection:CCTransitionDirectionDown duration:1]];
     [homeButton removeFromSuperview];
+}
+
+-(void)learnMore{
+    
+    NSURL *url = [ [ NSURL alloc ] initWithString: @"http://en.wikipedia.org/wiki/Graph_coloring" ];
+    [[UIApplication sharedApplication] openURL:url];
+    
 }
 
 
